4a0b0267954d1b6214e0ad1aff7099357066ff56
pia <schoolpia914@gmail.com>
2025-11-02 12:37:05 -0800

    Added function to find number of unsatisfied clauses for a given 3SAT problem and interpretation

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import numpy as np
 import random
 
 # create a 3SAT problem with c clauses of 3 prepositions out of n possible variables
@@ -10,7 +9,7 @@ def createSat(c, n):
         var2 = (random.randint(0,n), random.randint(0,1))
         var3 = (random.randint(0,n), random.randint(0,1))
         problem.append((var1,var2,var3)) # append clause to current problem list
-        problem = list(set(i for i in problem)) # use set logic to remove dups from list
+        problem = list(set(i for i in problem)) # use set logic to remove dupes from list
 
     return problem
 
@@ -36,3 +35,28 @@ cn7 = generateProblems(50,140,20)
 cn8 = generateProblems(50,160,20)
 cn9 = generateProblems(50,180,20)
 cn10 = generateProblems(50,200,20)
+
+
+# find the number of unsatisfied clauses for a given 3SAT problem and interpretation
+def numUnsatisfied(problem, interp):
+    clausesUnsatisfied = 0
+    conjuncTrue = False
+    for i in problem: # for each clause
+        disjuncTrue = False
+        for j in i: # for each prep in a clause
+            val = j[0]-1
+            if j[1] == 1: # if prep is negated
+                disjuncTrue = disjuncTrue or not interp[val]
+            else:
+                disjuncTrue = disjuncTrue or interp[val]
+        conjuncTrue = conjuncTrue and disjuncTrue
+        if disjuncTrue == False:
+            clausesUnsatisfied = clausesUnsatisfied + 1
+
+    return clausesUnsatisfied
+
+# testp = [((1, 0), (2, 0), (3, 0)), ((1, 1), (2, 0), (3, 0))]
+# testintep = [1, 0, 0]
+#
+# result = numUnsatisfied(testp, testintep)
+# print(result)
